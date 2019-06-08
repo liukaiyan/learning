80f20c4455670812f7f5563a3731eaf52af00166
liuky <631828268@qq.com>
2019-06-08 21:35:19 +0800

    third version

--- code.java
+++ code.java
@@ -1,4 +1,4 @@
-public class SingleThread extends Thread{
+/*public class SingleThread extends Thread{
 	int count;
 	public SingleThread(int count){
 		this.count = count;
@@ -6,6 +6,15 @@ public class SingleThread extends Thread{
 	public void run(){
 		System.out.println("Who are you?");
 	}
-}
+}*/
 
 //------------------The first modify--------------------
+//
+//
+//
+public class code{
+	public static void main(String[] args){
+		System.out.println("Haven't we met somewhere before?");
+	}
+//------------------The third version-------------------
+}
